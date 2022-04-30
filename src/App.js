5f3c71a98ac139998d67cc08899013d2ba17b3f4
PalashDas11 <pnpalash20@gmail.com>
2022-04-30 21:31:31 +0600

    header and using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home/Home';
 import Blog from './component/Blog/Blog';
 import Inventory from './component/Inventory/Inventory';
 import Header from './component/Header/Header';
+import NotFound from './component/NotFound/NotFound';
 
 function App() {
   return (
@@ -14,6 +17,7 @@ function App() {
        <Route path ="/" element ={<Home></Home>}></Route>
        <Route path = "/inventory" element = {<Inventory></Inventory>}></Route>
        <Route path = "/blog" element ={<Blog></Blog>} ></Route>
+       <Route path = "*" element = {<NotFound></NotFound>}></Route>
 
      </Routes>
     </div>
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 
 const Header = () => {
     return (
-        <div>
-            <h2>this is header</h2>
-        </div>
+        <>
+
+            <Navbar collapseOnSelect expand="lg" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#home" as={Link} to="/">
+                        <span>Omega</span>
+                        <span> Grocery</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto nav-items">
+                            <Nav.Link href="#home" as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link href="#inventory" as={Link} to="/inventory">Inventory</Nav.Link>
+                            <Nav.Link href="#blog" as={Link} to="/blog">Blog</Nav.Link>
+                            {/* {
+                                user ? 
+                                <Nav.Link href="#register" as={Link} to="/register" className='custom-btn2 'onClick={handleSignOut}>Sign Out</Nav.Link>
+                                :
+                                <Nav.Link href="#login" as={Link} to="/login" className='custom-btn2 '>Login</Nav.Link>
+                            } */}
+                            
+
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
    
     );
 };
