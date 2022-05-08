2880ea0478015bfb8f1e6b9332d37bb8183a1141
PalashDas11 <pnpalash20@gmail.com>
2022-05-08 17:04:38 +0600

    try somthing

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,6 +6,21 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+// don't working when i using env.local file..
+
+// const firebaseConfig = {
+//   apiKey:  process.env.REACT_APP_apiKey,
+//   authDomain:  process.env.REACT_APP_authDomain,
+//   projectId:  process.env.REACT_APP_projectId,
+//   storageBucket:  process.env.REACT_APP_storageBucket,
+//   messagingSenderId:  process.env.REACT_APP_messagingSenderId,
+//   appId:  process.env.REACT_APP_appId,
+//   measurementId: process.env.REACT_APP_measurementId
+
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyCi-Yt-3X0CNiQ7StmYv9ndv9xkanAl0DY",
   authDomain: "omega-grocery.firebaseapp.com",
